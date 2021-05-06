e0cd2a1bc77f8049fb2d9c97d7e8ed1b759df463
Cécile BONITEAU <proxyma-centauri@users.noreply.github.com>
2021-05-06 16:23:06 +0200

    Fixed initialisation of layer in forward for ReshapedBatchNorm

--- src/utils/normalization/ReshapedBatchNorm.py
+++ src/utils/normalization/ReshapedBatchNorm.py
@@ -5,12 +5,14 @@ class ReshapedBatchNorm(nn.Module):
     """ Performs BatchNormalization through BatchNorm3d,
         after having reshaped the data into a 5d Tensor"""
 
-    def __init__(self):
+    def __init__(self, 
+                in_channels: int,
+                group_dim: int):
         super(ReshapedBatchNorm, self).__init__()
+        self.BatchNormalization = nn.BatchNorm3d(in_channels * group_dim)
 
     def forward(self, x):
         bs, c, g, h, w, d = x.shape
         x = x.reshape([bs, c * g, h, w, d])
-        BatchNormalization = nn.BatchNorm3d(c * g)
-        x = (BatchNormalization(x)).view([bs, c, g, h, w, d])
+        x = (self.BatchNormalization(x)).view([bs, c, g, h, w, d])
         return x
