Fix elif instead of if in normalization choice (#17)
Co-authored-by: Cécile BONITEAU <proxyma-centauri@users.noreply.github.com>

src/convs.py
@@ -52,7 +52,7 @@ class ConvBlock(nn.Module):
         if normalization:
             if normalization == "bn":
                 modules.append(nn.BatchNorm3d(out_channels))
-            if normalization == "sn":
+            elif normalization == "sn":
                 modules.append(SwitchNorm3d(out_channels))
             else:
                 raise ValueError(

src/gconvs.py
@@ -193,7 +193,7 @@ class GconvBlock(nn.Module):
         if normalization:
             if normalization == "bn":
                 other_modules.append(ReshapedBatchNorm(out_channels, group_dim))
-            if normalization == "sn":
+            elif normalization == "sn":
                 other_modules.append(ReshapedSwitchNorm(out_channels, group_dim))
             else:
                 raise ValueError(