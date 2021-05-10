69e0749c9951991ff91e107fa3ac67e35182a9a4
Cécile BONITEAU <proxyma-centauri@users.noreply.github.com>
2021-05-10 10:31:08 +0200

    Added Switch normalization possibility

---
+++ src/utils/normalization/ReshapedSwitchNorm.py
@@ -0,0 +1,29 @@
+import torch
+from torch import nn
+from src.utils.normalization.SwitchNorm3d import SwitchNorm3d
+
+class ReshapedSwitchNorm(nn.Module):
+    """ Performs BatchNormalization through BatchNorm3d,
+        after having reshaped the data into a 5d Tensor"""
+
+    def __init__(self, 
+                in_channels: int,
+                group_dim: int,
+                eps: float=1e-5,
+                momentum: float=0.997,
+                using_moving_average: bool=True,
+                using_bn: bool=True,
+                last_gamma: bool=False):
+        super(ReshapedSwitchNorm, self).__init__()
+        self.SwitchNormalization = SwitchNorm3d(in_channels * group_dim,
+                                                eps,
+                                                momentum,
+                                                using_moving_average,
+                                                using_bn,
+                                                last_gamma)
+
+    def forward(self, x):
+        bs, c, g, h, w, d = x.shape
+        x = x.reshape([bs, c * g, h, w, d])
+        x = (self.SwitchNormalization(x)).view([bs, c, g, h, w, d])
+        return x
