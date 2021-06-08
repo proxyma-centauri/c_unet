Fixed match_channels convolutions arguments, fixed order of nonlinearity and normalization in convblocks

c_unet/layers/convs.py
@@ -40,18 +40,7 @@ class ConvBlock(nn.Module):
         modules = [
             conv3d(in_channels, out_channels, kernel_size, stride, padding,
                    bias, dilation)
-        ]
-
-        # ! WARNING: You'll end up using a batch size of 1, we need another
-        # ! normalization layer (e.g. switchnorm).
-        if normalization:
-            if normalization == "bn":
-                modules.append(nn.BatchNorm3d(out_channels))
-            elif normalization == "sn":
-                modules.append(SwitchNorm3d(out_channels))
-            else:
-                raise ValueError(
-                    f"Invalid normalization value: {normalization}")
+        ] 
 
         if nonlinearity:
             if nonlinearity == "relu":
@@ -62,6 +51,15 @@ class ConvBlock(nn.Module):
                 modules.append(nn.Softmax(dim=1))
             else:
                 raise ValueError(f"Invalid nonlinearity value: {nonlinearity}")
+
+        if normalization:
+            if normalization == "bn":
+                modules.append(nn.BatchNorm3d(out_channels))
+            elif normalization == "sn":
+                modules.append(SwitchNorm3d(out_channels))
+            else:
+                raise ValueError(
+                    f"Invalid normalization value: {normalization}")
 
         self.block = nn.Sequential(*modules)
 
@@ -124,8 +122,8 @@ class ConvResBlock(nn.Module):
                             stride=1,
                             padding=0,
                             dilation=1,
-                            bias=False,
-                            nonlinearity="",
+                            bias=bias,
+                            nonlinearity=nonlinearity,
                             normalization=""
         )
 

c_unet/layers/gconvs.py
@@ -187,17 +187,6 @@ class GconvBlock(nn.Module):
             self.b = nn.Parameter(torch.full((1,1), 0.01))
 
         other_modules = []
-
-        # ! WARNING: You'll end up using a batch size of 1, we need another
-        # ! normalization layer (e.g. switchnorm).
-        if normalization:
-            if normalization == "bn":
-                other_modules.append(ReshapedBatchNorm(out_channels, group_dim))
-            elif normalization == "sn":
-                other_modules.append(ReshapedSwitchNorm(out_channels, group_dim))
-            else:
-                raise ValueError(
-                    f"Invalid normalization value: {normalization}")
         
         if nonlinearity:
             if nonlinearity == "relu":
@@ -208,6 +197,15 @@ class GconvBlock(nn.Module):
                 other_modules.append(nn.Softmax(dim=1))
             else:
                 raise ValueError(f"Invalid nonlinearity value: {nonlinearity}")
+
+        if normalization:
+            if normalization == "bn":
+                other_modules.append(ReshapedBatchNorm(out_channels, group_dim))
+            elif normalization == "sn":
+                other_modules.append(ReshapedSwitchNorm(out_channels, group_dim))
+            else:
+                raise ValueError(
+                    f"Invalid normalization value: {normalization}")
 
         self.OtherModules = nn.Sequential(*other_modules)
 
@@ -282,8 +280,8 @@ class GconvResBlock(nn.Module):
                             padding=0,
                             dilation=1,
                             dropout=0,
-                            bias=False,
-                            nonlinearity="",
+                            bias=bias,
+                            nonlinearity=nonlinearity,
                             normalization=""
         )
 