055d7bfd399ab4b79190576fa349a0237b4071eb
Clément POIRET <poiret.clement@outlook.fr>
2021-07-02 15:13:58 +0200

    Testing GMaxPool3d

--- c_unet/utils/pooling/ReshapedMaxPool.py
+++ c_unet/utils/pooling/ReshapedMaxPool.py
@@ -1,5 +1,6 @@
 import logging
 
+from einops import rearrange
 from torch import nn
 from typing import List, Union
 
@@ -29,3 +30,22 @@ class ReshapedMaxPool(nn.Module):
         _, _, new_h, new_w, new_d = x.shape
         x = x.view([bs, c, g, new_h, new_w, new_d])
         return x
+    
+    
+    class GMaxPool3d(nn.Module):
+        """Pool over channels"""
+        def __init__(self,
+                     num_group: int = 4,
+                     kernel_size: int = 3,
+                     stride: Union[int, List[int]] = 1,
+                     padding: Union[str, int] = 1):
+            super(ReshapedMaxPool, self).__init__()
+            self.g = num_group
+            self.pool = nn.MaxPool3d(kernel_size,
+                                     stride,
+                                     padding)
+            
+        def forward(self, x):
+            x = rearrange(x, "b c g h w d -> (b g) c h w d")
+            x = self.pool(x)
+            return rearrange(x, "(b g) c h w d -> b c g h w d", g=g)
