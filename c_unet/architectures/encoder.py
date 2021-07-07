Fix/vanishing gradients (#37)
* Tests on gconv

* Cleaned up conv modifications

* Added nonlinearities and rebaed on main

* Updated LightningUnet model with backward improvements and tensorboard logging

* Solved pooling error and kept most useful fucntion

* Update README

* Small fix in loss logging

* Fix W and grid device mismatch in gpu mode

* Better device assignation for get_rot_mat

* Remove useless logging

* Remove augment for now

* Updated groups T4 and S4

* Made gradients logging optional

Co-authored-by: Cécile BONITEAU <proxyma-centauri@users.noreply.github.com>

c_unet/architectures/encoder.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 
 from c_unet.layers.gconvs import GconvResBlock
 from c_unet.layers.convs import ConvResBlock
-from c_unet.utils.pooling.ReshapedMaxPool import ReshapedMaxPool
+from c_unet.utils.pooling.GPool3d import GPool3d
 
 
 class EncoderBlock(nn.Module):
@@ -21,6 +21,8 @@ class EncoderBlock(nn.Module):
         - pool_size (int): Size of the pooling kernel. Defaults to 2.
         - pool_stride (Union[int, List[int]]): Stride of the pooling. Defaults to 2.
         - pool_padding (Union[str, int]): Zero-padding added to all three sides of the input at pooling. Defaults to 0.
+        - pool_reduction : Type of pooling for G-CNNs. Defualts to "max".
+        - pool_factor : For G-cnns, reduction factor of the pooling. Defaults to 2.
 
         - dropout (float, optional) : Value of dropout to use. Defaults to 0.1
         - bias (bool, optional): If True, adds a learnable bias to the output. Defaults to True.
@@ -39,98 +41,94 @@ class EncoderBlock(nn.Module):
         ValueError: Invalid normalization value
         ValueError: Invalid nonlinearity value
     """
-    def __init__(self, 
-                # Channels
-                in_channels: int, 
-                # Kernel arguments
-                kernel_size: int = 3,
-                stride: Union[int, List[int]] = 1,
-                padding: Union[str, int] = 1,
-                # Pooling
-                pool_size: int=2,
-                pool_stride: Union[int, List[int]] = 2,
-                pool_padding: Union[str, int] = 0,
-                # Convolution arguments
-                dropout: Optional[bool] = 0.1,
-                bias: bool = True,
-                dilation: int = 1,
-                nonlinearity: Optional[str] = "relu",
-                normalization: Optional[str] = "bn",
-                # Model
-                model_depth: int=4,
-                root_feat_maps: int = 32,
-                # Group arguments (by default, no group)
-                group: Union[str, None]=None,
-                group_dim: int=0):
+    def __init__(
+            self,
+            # Channels
+            in_channels: int,
+            # Kernel arguments
+            kernel_size: int = 3,
+            stride: Union[int, List[int]] = 1,
+            padding: Union[str, int] = 1,
+            # Pooling
+            pool_size: Optional[int] = 2,
+            pool_stride: Optional[Union[str, int]] = 2,
+            pool_padding: Optional[Union[str, int]] = 0,
+            pool_reduction: Optional[str] = "mean",
+            pool_factor: Optional[int] = 2,
+            # Convolution arguments
+            dropout: Optional[bool] = 0.1,
+            bias: bool = True,
+            dilation: int = 1,
+            nonlinearity: Optional[str] = "relu",
+            normalization: Optional[str] = "bn",
+            # Model
+            model_depth: int = 4,
+            root_feat_maps: int = 32,
+            # Group arguments (by default, no group)
+            group: Union[str, None] = None,
+            group_dim: int = 0):
         super(EncoderBlock, self).__init__()
 
         self.root_feat_maps = root_feat_maps
         self.logger = logging.getLogger(__name__)
 
-
         self.module_dict = nn.ModuleDict()
 
         # U-net structure
         for depth in range(model_depth):
-            feat_map_channels = 2 ** (depth + 1) * self.root_feat_maps
-            
+            feat_map_channels = 2**(depth + 1) * self.root_feat_maps
+
             if group:
                 is_first_conv = True if (depth == 0) else False
                 self.conv_block = GconvResBlock(group,
-                                        group_dim,
-                                        in_channels,
-                                        feat_map_channels,
-                                        feat_map_channels,
-                                        is_first_conv,
-                                        kernel_size,
-                                        stride,
-                                        padding,
-                                        dilation=dilation,
-                                        dropout=dropout,
-                                        bias=bias,
-                                        nonlinearity=nonlinearity,
-                                        normalization=normalization)
+                                                group_dim,
+                                                in_channels,
+                                                feat_map_channels,
+                                                feat_map_channels,
+                                                is_first_conv,
+                                                kernel_size,
+                                                stride,
+                                                padding,
+                                                dilation=dilation,
+                                                dropout=dropout,
+                                                bias=bias,
+                                                nonlinearity=nonlinearity,
+                                                normalization=normalization)
             else:
                 self.conv_block = ConvResBlock(in_channels,
-                                        feat_map_channels,
-                                        feat_map_channels,
-                                        kernel_size,
-                                        stride,
-                                        padding,
-                                        bias=bias,
-                                        dilation=dilation,
-                                        nonlinearity=nonlinearity,
-                                        normalization=normalization)
+                                               feat_map_channels,
+                                               feat_map_channels,
+                                               kernel_size,
+                                               stride,
+                                               padding,
+                                               bias=bias,
+                                               dilation=dilation,
+                                               nonlinearity=nonlinearity,
+                                               normalization=normalization)
 
             self.module_dict[f"conv_block_{depth}"] = self.conv_block
 
             in_channels, feat_map_channels = feat_map_channels, feat_map_channels * 2
 
             if depth == 1:
-                self.dilated_dense = DilatedDenseBlock(in_channels,
-                                            root_feat_maps*2,
-                                            in_channels,
-                                            kernel_size,
-                                            stride,
-                                            dropout,
-                                            bias,
-                                            nonlinearity,
-                                            normalization,
-                                            3,
-                                            2,
-                                            group,
-                                            group_dim)
+                self.dilated_dense = DilatedDenseBlock(
+                    in_channels, root_feat_maps * 2, in_channels, kernel_size,
+                    stride, dropout, bias, nonlinearity, normalization, 3, 2,
+                    group, group_dim)
 
             if depth == model_depth - 1:
                 break
             else:
                 if group:
-                    self.pooling = ReshapedMaxPool(kernel_size=pool_size, stride=pool_stride, padding=pool_padding)
+                    self.pooling = GPool3d(pool_over="hwd",
+                                           reduction=pool_reduction,
+                                           reduction_factor=pool_factor)
                 else:
-                    self.pooling = nn.MaxPool3d(kernel_size=pool_size, stride=pool_stride, padding=pool_padding)
-                
-                self.module_dict[f"max_pooling_{depth}"] = self.pooling
+                    self.pooling = nn.MaxPool3d(kernel_size=pool_size,
+                                                stride=pool_stride,
+                                                padding=pool_padding)
 
+                self.module_dict[f"max_pooling_{depth}"] = self.pooling
 
     def forward(self, x):
         down_sampling_features = []
@@ -138,7 +136,7 @@ class EncoderBlock(nn.Module):
             if key.startswith("conv"):
                 x = layer(x)
                 self.logger.debug(f"{key}, {x.shape}")
-                if key.endswith("1"): # Layer 1
+                if key.endswith("1"):  # Layer 1
                     x = self.dilated_dense(x)
                 down_sampling_features.append(x)
             elif key.startswith("max_pooling"):

c_unet/groups/T4_group.py
@@ -3,7 +3,9 @@ import logging
 import numpy as np
 
 import torch
+import torch.nn.functional as F
 import torchvision.transforms.functional as torchtransforms
+
 
 class T4_group(object):
     def __init__(self):
@@ -11,28 +13,38 @@ class T4_group(object):
         self.group_dim = 12
         self.cayleytable = self.get_cayleytable()
 
-    
-    def rotate(self, x, axis, shift):
-        angles = [0.,np.pi/2.,np.pi,3.*np.pi/2.]
-        perm = ([0,3,2,1],[0,1,3,2],[0,2,1,3])
-        x = x.permute(perm[axis])
-        x = torchtransforms.rotate(x, angles[shift])
-        return x.permute(perm[axis])
+    def get_rot_mat(self, theta, device):
+        theta = torch.tensor(theta)
+        rot_mat = torch.tensor([[torch.cos(theta), -torch.sin(theta), 0],
+                                [torch.sin(theta),
+                                 torch.cos(theta), 0]]).to(device)
+        return rot_mat
 
+    def rot_img(self, x, theta):
+        rot_mat = self.get_rot_mat(theta,
+                                   x.device)[None,
+                                             ...].repeat(x.shape[0], 1, 1)
+        grid = F.affine_grid(rot_mat, x.size())
+        x_rot = F.grid_sample(x, grid)
+        return x_rot
+
+    def rotate(self, x, axis, shift):
+        angles = [0., np.pi / 2., np.pi, 3. * np.pi / 2.]
+        perm = ([0, 3, 2, 1], [0, 1, 3, 2], [0, 2, 1, 3])
+        x = x.permute(perm[axis])
+        x = self.rot_img(x, angles[shift])
+        return x.permute(perm[axis])
 
     def r1(self, x):
         x = self.rotate(x, 0, -1)
         return self.rotate(x, 1, 1)
 
-        
     def r2(self, x):
         x = self.rotate(x, 0, -1)
         return self.rotate(x, 1, -1)
 
-
     def r3(self, x):
         return self.rotate(x, 0, 2)
-
 
     def get_Grotations(self, x):
         """Rotate the tensor x with all 12 T4 rotations
@@ -46,19 +58,20 @@ class T4_group(object):
         for i in range(3):
             y = x
             for __ in range(i):
-                y = self.r1(y) 
+                y = self.r1(y)
             for j in range(3):
                 z = y
                 for __ in range(j):
+                    if z.shape[0] == 0:
+                        print(j)
                     z = self.r2(z)
                 Z.append(z)
         for i in range(3):
             z = self.r3(x)
             for __ in range(i):
-                z = self.r2(z) 
+                z = self.r2(z)
             Z.append(z)
         return Z
-
 
     def G_permutation(self, W):
         """Permute the outputs of the group convolution
@@ -70,17 +83,29 @@ class T4_group(object):
         """
         Wsh = W.shape
         cayley = self.cayleytable
+        Wsh = W.shape
         U = []
         for i in range(12):
             perm_mat = self.get_permutation_matrix(cayley, i).to(W.device)
-            w = W[:,:,:,:,:,:,i]
-            w = w.permute([0,1,2,3,5,4])
+            w = W[i, :, :, :, :, :, :]
             w = w.reshape([-1, 12])
             w = torch.matmul(w, perm_mat)
-            w = w.view(list(Wsh[:4])+[-1,12])
-            U.append(w.permute([0,1,2,3,5,4]))
+            w = w.view([-1] + list(Wsh[2:]))
+            U.append(w)
         return U
 
+        # Wsh = W.shape
+        # cayley = self.cayleytable
+        # U = []
+        # for i in range(12):
+        #     perm_mat = self.get_permutation_matrix(cayley, i).to(W.device)
+        #     w = W[:, :, :, :, :, :, i]
+        #     w = w.permute([0, 1, 2, 3, 5, 4])
+        #     w = w.reshape([-1, 12])
+        #     w = torch.matmul(w, perm_mat)
+        #     w = w.view(list(Wsh[:4]) + [-1, 12])
+        #     U.append(w.permute([0, 1, 2, 3, 5, 4]))
+        # return U
 
     def get_permutation_matrix(self, perm, dim):
         """Creates and return the permutation matrix
@@ -94,50 +119,41 @@ class T4_group(object):
         ndim = perm.shape[0]
         mat = np.zeros((ndim, ndim))
         for j in range(ndim):
-            mat[j,perm[j,dim]] = 1
+            mat[j, perm[j, dim]] = 1
         return torch.from_numpy(mat).float()
-
 
     def get_t4mat(self):
         Z = []
         for i in range(3):
             y = np.eye(3)
             for __ in range(i):
-                y = y @ self.get_3Drotmat(1,0,0)   
-                y = y @ self.get_3Drotmat(0,-1,0)  
+                y = y @ self.get_3Drotmat(1, 0, 0)
+                y = y @ self.get_3Drotmat(0, -1, 0)
             for j in range(3):
                 z = y
                 for __ in range(j):
                     #z = r2(z)
-                    z = z @ self.get_3Drotmat(1,0,0)  
-                    z = z @ self.get_3Drotmat(0,1,0) 
+                    z = z @ self.get_3Drotmat(1, 0, 0)
+                    z = z @ self.get_3Drotmat(0, 1, 0)
                 Z.append(z)
         for i in range(3):
             #z = r3(x)
-            z = self.get_3Drotmat(2,0,0) 
+            z = self.get_3Drotmat(2, 0, 0)
             for __ in range(i):
-                #z = r2(z) 
-                z = z @ self.get_3Drotmat(1,0,0) 
-                z = z @ self.get_3Drotmat(0,1,0)  
+                #z = r2(z)
+                z = z @ self.get_3Drotmat(1, 0, 0)
+                z = z @ self.get_3Drotmat(0, 1, 0)
             Z.append(z)
         return Z
 
+    def get_3Drotmat(self, x, y, z):
+        c = [1., 0., -1., 0.]
+        s = [0., 1., 0., -1]
 
-    def get_3Drotmat(self,x,y,z):
-        c = [1.,0.,-1.,0.]
-        s = [0.,1.,0.,-1]
-
-        Rx = np.asarray([[c[x],     -s[x],  0.],
-                         [s[x],     c[x],   0.],
-                         [0.,       0.,     1.]])
-        Ry = np.asarray([[c[y],     0.,     s[y]],
-                         [0.,       1.,     0.],
-                         [-s[y],    0.,     c[y]]])
-        Rz = np.asarray([[1.,       0.,     0.],
-                         [0.,       c[z],   -s[z]],
-                         [0.,       s[z],   c[z]]])
+        Rx = np.asarray([[c[x], -s[x], 0.], [s[x], c[x], 0.], [0., 0., 1.]])
+        Ry = np.asarray([[c[y], 0., s[y]], [0., 1., 0.], [-s[y], 0., c[y]]])
+        Rz = np.asarray([[1., 0., 0.], [0., c[z], -s[z]], [0., s[z], c[z]]])
         return Rz @ Ry @ Rx
-
 
     def get_cayleytable(self):
         """Returns the Cayley table of V group
@@ -154,6 +170,4 @@ class T4_group(object):
                     if np.sum(np.square(el - r)) < 1e-6:
                         cayley.append(i)
         cayley = np.stack(cayley)
-        return np.reshape(cayley, [12,12]).T
-
-
+        return np.reshape(cayley, [12, 12]).T

c_unet/groups/V_group.py
@@ -1,9 +1,10 @@
 import logging
 
 import numpy as np
+import torch.nn.functional as F
 
 import torch
-import torchvision.transforms.functional as torchtransforms
+from einops import rearrange
 
 
 class V_group(object):
@@ -18,64 +19,77 @@ class V_group(object):
         Returns:
             4 by 4 numpy array
         """
-        cayley = np.asarray([[0,1,2,3],
-                             [1,0,3,2],
-                             [2,3,0,1],
-                             [3,2,1,0]])
-        return cayley        
+        cayley = np.asarray([[0, 1, 2, 3], [1, 0, 3, 2], [2, 3, 0, 1],
+                             [3, 2, 1, 0]])
+        return cayley
 
+    def get_rot_mat(self, theta, device):
+        theta = torch.tensor(theta)
+        rot_mat = torch.tensor([[torch.cos(theta), -torch.sin(theta), 0],
+                                [torch.sin(theta),
+                                 torch.cos(theta), 0]]).to(device)
+        return rot_mat
 
-    def get_Grotations(self, x):
+    def rot_img(self, x, theta):
+        rot_mat = self.get_rot_mat(theta,
+                                   x.device)[None,
+                                             ...].repeat(x.shape[0], 1, 1)
+        grid = F.affine_grid(rot_mat, x.size())
+        x_rot = F.grid_sample(x, grid)
+        return x_rot
+
+    def get_Grotations(self, W):
         """Rotate the tensor x with all 4 Klein Vierergruppe rotations
 
         Args:
-            x: [n_channels, h,w,d]
+            W: [n_channels, h,w,d]
         Returns:
             list of 4 rotations of x [[n_channels,h,w,d],....]
         """
-        xsh = x.shape
-        angles = [0.,np.pi]
-        rx = []
-        for i in range(2):
+        angles = [0., np.pi]
+
+        Wrots = []
+
+        for z_angle in angles:
             # 2x 180. rotations about the z axis
-            perm = [0,2,1,3]
-            y = x.permute(perm)
+            perm = [0, 2, 1, 3]
 
-            y = torchtransforms.rotate(y, angles[i]) # dims
-
-            y = y.permute(perm)
+            Wz_1 = W.permute(perm)
+            Wz_2 = self.rot_img(Wz_1, z_angle)
+            W_rot_around_z = Wz_2.permute(perm)
 
             # 2x 180. rotations about another axis
-            for j in range(2):
-                perm = [0,3,2,1]
-                z = y.permute(perm)
-                z = torchtransforms.rotate(z, angles[j])
-                z = z.permute(perm)
-                rx.append(z)
-        return rx
+            for y_angle in angles:
+                perm = [0, 3, 2, 1]
 
+                Wy_1 = W_rot_around_z.permute(perm)
+                Wy_2 = self.rot_img(Wy_1, y_angle)
+                W_rot_around_both_axis = Wy_2.permute(perm)
+
+                Wrots.append(W_rot_around_both_axis)
+
+        return Wrots
 
     def G_permutation(self, W):
         """Permute the outputs of the group convolution
         
         Args:
-            W: [n_channels_in, h, w, d, group_dim, n_channels_out, group_dim]
+            W: [group_dim, n_channels_in, group_dim, n_channels_out, h, w, d]
         Returns:
-            list of 4 permutations of W [[n_channels_in, h, w, d, group_dim, n_channels_out, group_dim],....]
+            list of 4 permutations of W [[group_dim, n_channels_in, group_dim, n_channels_out, h, w, d],....]
         """
         Wsh = W.shape
         cayley = self.cayleytable
+        Wsh = W.shape
         U = []
         for i in range(4):
             perm_mat = self.get_permutation_matrix(cayley, i).to(W.device)
-            w = W[:,:,:,:,:,:,i]
-            w = w.permute([0,1,2,3,5,4])
+            w = W[i, :, :, :, :, :, :]
             w = w.reshape([-1, 4])
             w = torch.matmul(w, perm_mat)
-            w = w.view(list(Wsh[:4])+[-1,4]) 
-            U.append(w.permute([0,1,2,3,5,4]))
+            w = w.view([-1] + list(Wsh[2:]))
+            U.append(w)
         return U
-
 
     def get_permutation_matrix(self, perm, dim):
         """Creates and return the permutation matrix
@@ -89,5 +103,5 @@ class V_group(object):
         ndim = perm.shape[0]
         mat = np.zeros((ndim, ndim))
         for j in range(ndim):
-            mat[j,perm[j,dim]] = 1
+            mat[j, perm[j, dim]] = 1
         return torch.from_numpy(mat).float()

c_unet/layers/gconvs.py
@@ -1,20 +1,19 @@
-
 import logging
-import torch
-import torch.nn.functional as F
-
-from torch import nn
 from typing import List, Optional, Union
 
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+from c_unet.layers.convs import ConvBlock
 from c_unet.utils.dropout.GaussianDropout import GaussianDropout
 from c_unet.utils.normalization.ReshapedBatchNorm import ReshapedBatchNorm
 from c_unet.utils.normalization.ReshapedSwitchNorm import ReshapedSwitchNorm
 
-from c_unet.layers.convs import ConvBlock
 
 class Gconv3d(nn.Module):
     """Performs a discretized convolution on SO(3)
-
+ 
     Args:
         group (str): Shorthand name representing the group to use
         expected_group_dim (int): Expected group dimension, it is 
@@ -26,12 +25,11 @@ class Gconv3d(nn.Module):
         stride (Union[int, List[int]], optional): Stride of the convolution. Defaults to 1.
         padding (Union[str, int], optional): Zero-padding added to all three sides of the input. Defaults to 1.
         dropout (float, optional): Value of dropout to use. Defaults to 0.1.
-
+ 
     Raises:
         ValueError: Invalid padding value
         ValueError: Unrecognized group
     """
-
     def __init__(self,
                  group: str,
                  expected_group_dim: int,
@@ -77,11 +75,10 @@ class Gconv3d(nn.Module):
         # R^2. For a p4 convolution over p4, we rotate it, and then shift up by
         # one dimension in the channels.
         self.W = nn.Parameter(
-            torch.empty([
-                in_channels * expected_group_dim * out_channels, kernel_size,
-                kernel_size, kernel_size
-            ]))
-        nn.init.kaiming_normal_(self.W, nonlinearity="relu")
+            torch.Tensor(in_channels * expected_group_dim * out_channels,
+                         kernel_size, kernel_size, kernel_size))
+
+        torch.nn.init.xavier_uniform_(self.W)
 
         self.dropout = GaussianDropout(p=dropout)
 
@@ -92,40 +89,32 @@ class Gconv3d(nn.Module):
         # rotated and stacked into a much bigger kernel
         x = x.reshape(bs, c * g, h, w, d)
 
-        WN = self.group.get_Grotations(self.W)
+        WN = self.group.get_Grotations(self.W.clone().to(x.device))
         WN = torch.stack(WN, 0)
 
         if g == 1:
-            # A convolution on R^2 is just standard convolution with 3 extra
-            # output channels for each rotation of the filters
-            WN = WN.view(-1, self.in_channels, self.kernel_size,
-                         self.kernel_size, self.kernel_size)
-        elif g == self.group_dim:
-            # A convolution on p4 is different to convolution on R^2. For each
-            # dimension of the group output, we need to both rotate the filters
-            # and circularly shift them in the input-group dimension. In a
-            # sense, we have to spiral the filters
-            WN = WN.view(self.in_channels, self.kernel_size, self.kernel_size,
-                         self.kernel_size, self.group_dim, self.out_channels,
-                         self.group_dim)
+            WN = WN.view(self.out_channels * self.group_dim, self.in_channels,
+                         self.kernel_size, self.kernel_size, self.kernel_size)
+        else:
+            WN = WN.view(self.group_dim, self.in_channels, self.group_dim,
+                         self.out_channels, self.kernel_size, self.kernel_size,
+                         self.kernel_size)
 
             WN_shifted = self.group.G_permutation(WN)
             WN = torch.stack(WN_shifted, -1)
 
-            # Shift over axis 6
-            # Stack the shifted tensors and reshape to 4D kernel
             WN = WN.view(self.out_channels * self.group_dim,
                          self.in_channels * self.group_dim, self.kernel_size,
                          self.kernel_size, self.kernel_size)
-        else:
-            raise ValueError(f"`group_dim` ({g}) doesn't match input data")
 
-        # Convolve
         # Gaussian dropout on the weights
         WN = self.dropout(WN)
 
-        # TODO: check if we really need padding like `reflect` or `valid`
-        x = F.conv3d(x, WN, stride=self.stride, padding=self.p, dilation=self.dilation)
+        x = F.conv3d(x,
+                     WN,
+                     stride=self.stride,
+                     padding=self.p,
+                     dilation=self.dilation)
         _, _, h, w, d = x.shape
         x = x.view(bs, self.out_channels, self.group_dim, h, w, d)
 
@@ -156,43 +145,39 @@ class GconvBlock(nn.Module):
         ValueError: Invalid nonlinearity value
     """
     def __init__(self,
-                group: str,
-                expected_group_dim: int,
-                in_channels: int,
-                out_channels: int,
-                kernel_size: int = 3,
-                stride: Union[int, List[int]] = 1,
-                padding: Union[str, int] = 1,
-                dilation: int = 1,
-                dropout: float = 0.1,
-                bias: Optional[bool] = True,
-                nonlinearity: Optional[str] = "relu",
-                normalization: Optional[str] = "bn"):
+                 group: str,
+                 expected_group_dim: int,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: int = 3,
+                 stride: Union[int, List[int]] = 1,
+                 padding: Union[str, int] = 1,
+                 dilation: int = 1,
+                 dropout: float = 0.1,
+                 bias: Optional[bool] = True,
+                 nonlinearity: Optional[str] = "relu",
+                 normalization: Optional[str] = "bn"):
         super(GconvBlock, self).__init__()
         self.bias = bias
-        
-        self.Gconv = Gconv3d(group,
-                    expected_group_dim,
-                    in_channels,
-                    out_channels,
-                    kernel_size,
-                    dilation,
-                    stride,
-                    padding,
-                    dropout)
+
+        self.Gconv = Gconv3d(group, expected_group_dim, in_channels,
+                             out_channels, kernel_size, dilation, stride,
+                             padding, dropout)
 
         group_dim = self.Gconv.group_dim
 
         if bias:
-            self.b = nn.Parameter(torch.full((1,1), 0.01))
+            self.b = nn.Parameter(torch.full((1, 1), 0.01))
 
         other_modules = []
-        
+
         if nonlinearity:
             if nonlinearity == "relu":
                 other_modules.append(nn.ReLU(inplace=True))
             elif nonlinearity == "leaky-relu":
                 other_modules.append(nn.LeakyReLU(inplace=True))
+            elif nonlinearity == "elu":
+                other_modules.append(nn.ELU(inplace=True))
             elif nonlinearity == "sigmoid":
                 other_modules.append(nn.Sigmoid())
             elif nonlinearity == "softmax":
@@ -202,9 +187,11 @@ class GconvBlock(nn.Module):
 
         if normalization:
             if normalization == "bn":
-                other_modules.append(ReshapedBatchNorm(out_channels, group_dim))
+                other_modules.append(ReshapedBatchNorm(out_channels,
+                                                       group_dim))
             elif normalization == "sn":
-                other_modules.append(ReshapedSwitchNorm(out_channels, group_dim))
+                other_modules.append(
+                    ReshapedSwitchNorm(out_channels, group_dim))
             else:
                 raise ValueError(
                     f"Invalid normalization value: {normalization}")
@@ -250,22 +237,22 @@ class GconvResBlock(nn.Module):
         ValueError: Invalid nonlinearity value
     """
     def __init__(self,
-                group: str,
-                group_dim: int,
-                in_channels: int,
-                inter_channels: int,
-                out_channels: int,
-                is_first_conv: bool = False,
-                kernel_size: int = 3,
-                stride: Union[int, List[int]] = 1,
-                padding: Union[str, int] = 1,
-                dilation: int = 1,
-                dropout: float = 0.1,
-                first_kernel_size: Optional[int] = None,
-                first_padding: Optional[int] = None,
-                bias: Optional[bool] = True,
-                nonlinearity: Optional[str] = "relu",
-                normalization: Optional[str] = "bn"):
+                 group: str,
+                 group_dim: int,
+                 in_channels: int,
+                 inter_channels: int,
+                 out_channels: int,
+                 is_first_conv: bool = False,
+                 kernel_size: int = 3,
+                 stride: Union[int, List[int]] = 1,
+                 padding: Union[str, int] = 1,
+                 dilation: int = 1,
+                 dropout: float = 0.1,
+                 first_kernel_size: Optional[int] = None,
+                 first_padding: Optional[int] = None,
+                 bias: Optional[bool] = True,
+                 nonlinearity: Optional[str] = "relu",
+                 normalization: Optional[str] = "bn"):
         super(GconvResBlock, self).__init__()
         self.logger = logging.getLogger(__name__)
 
@@ -274,48 +261,50 @@ class GconvResBlock(nn.Module):
         other_padding = first_padding if first_padding is not None else padding
 
         self.match_channels = GconvBlock(group,
-                            expected_group_dim,
-                            in_channels,
-                            out_channels,
-                            kernel_size=1,
-                            stride=1,
-                            padding=0,
-                            dilation=1,
-                            dropout=0,
-                            bias=bias,
-                            nonlinearity=nonlinearity,
-                            normalization=""
-        )
+                                         expected_group_dim,
+                                         in_channels,
+                                         out_channels,
+                                         kernel_size=1,
+                                         stride=1,
+                                         padding=0,
+                                         dilation=1,
+                                         dropout=0,
+                                         bias=bias,
+                                         nonlinearity=nonlinearity,
+                                         normalization="")
 
         self.G_block_1 = GconvBlock(group,
-                            expected_group_dim,
-                            in_channels,
-                            inter_channels,
-                            other_kernel_size,
-                            stride,
-                            other_padding,
-                            dilation,
-                            dropout,
-                            bias=bias,
-                            nonlinearity=nonlinearity,
-                            normalization=normalization
-        )
-        self.G_block_2 = GconvBlock(group,
-                            group_dim,
-                            inter_channels,
-                            out_channels,
-                            kernel_size,
-                            stride,
-                            padding,
-                            dilation,
-                            dropout,
-                            bias=bias,
-                            nonlinearity="", # No nonlinearity
-                            normalization=normalization
-        )
+                                    expected_group_dim,
+                                    in_channels,
+                                    inter_channels,
+                                    other_kernel_size,
+                                    stride,
+                                    other_padding,
+                                    dilation,
+                                    dropout,
+                                    bias=bias,
+                                    nonlinearity=nonlinearity,
+                                    normalization=normalization)
+        self.G_block_2 = GconvBlock(
+            group,
+            group_dim,
+            inter_channels,
+            out_channels,
+            kernel_size,
+            stride,
+            padding,
+            dilation,
+            dropout,
+            bias=bias,
+            nonlinearity="",  # No nonlinearity
+            normalization=normalization)
 
-        self.relu = nn.ReLU()
-        
+        if nonlinearity == "leaky-relu":
+            self.relu = nn.LeakyReLU(inplace=True)
+        elif nonlinearity == "elu":
+            self.relu = nn.ELU(inplace=True)
+        else:
+            self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         z = self.match_channels(x)
@@ -336,27 +325,24 @@ class FinalGroupConvolution(nn.Module):
         - out_channels (int) Number of output channels
         - final_activation (str) : Final activation layer
     """
-    def __init__(self,
-                group_convolution: nn.Module,
-                group_dim: int,
-                out_channels: int,
-                final_activation: str):
+    def __init__(self, group_convolution: nn.Module, group_dim: int,
+                 out_channels: int, final_activation: str):
         super(FinalGroupConvolution, self).__init__()
 
         self.g_conv = group_convolution
-        self.reshaping_conv = ConvBlock(out_channels*group_dim,
-                                    out_channels,
-                                    kernel_size=1,
-                                    padding=0,
-                                    nonlinearity=final_activation,
-                                    normalization="")
+        self.reshaping_conv = ConvBlock(out_channels * group_dim,
+                                        out_channels,
+                                        kernel_size=1,
+                                        padding=0,
+                                        nonlinearity=final_activation,
+                                        normalization="")
 
     def forward(self, x):
         x = self.g_conv(x)
 
         # Reshaping input
         bs, c, g, h, w, d = x.shape
-        x = x.reshape(bs, c*g, h, w, d)
+        x = x.reshape(bs, c * g, h, w, d)
 
         # Removing group dimension
         x = self.reshaping_conv(x)

c_unet/training/datamodule.py
@@ -18,12 +18,12 @@ class DataModule(pl.LightningDataModule):
         - num_workers (int): number of workers for the dataloaders. Defaults to 0
         - train_val_ratio (float): ratio of the data to use in validation. Defaults to 0.7
     """
-    def __init__(self, 
-                    task: str,
-                    subset_name: str = "",
-                    batch_size: int = 16,
-                    num_workers: int = 0,
-                    train_val_ratio: float = 0.7):
+    def __init__(self,
+                 task: str,
+                 subset_name: str = "",
+                 batch_size: int = 16,
+                 num_workers: int = 0,
+                 train_val_ratio: float = 0.7):
         super().__init__()
         self.task = task
         self.subset_name = subset_name
@@ -38,16 +38,16 @@ class DataModule(pl.LightningDataModule):
         self.train_set = None
         self.val_set = None
         self.test_set = None
-    
+
     def get_max_shape(self, subjects):
         dataset = tio.SubjectsDataset(subjects)
         shapes = np.array([s.get_first_image().spatial_shape for s in dataset])
         return shapes.max(axis=0)
-    
-    def download_data(self):
 
+    def download_data(self):
         def get_niis(d):
-            return sorted(p for p in d.glob(f'*{self.subset_name}.nii*') if not p.name.startswith('.'))
+            return sorted(p for p in d.glob(f'*{self.subset_name}.nii*')
+                          if not p.name.startswith('.'))
 
         image_training_paths = get_niis(self.dataset_dir / 'imagesTr')
         label_training_paths = get_niis(self.dataset_dir / 'labelsTr')
@@ -56,34 +56,33 @@ class DataModule(pl.LightningDataModule):
         return image_training_paths, label_training_paths, image_test_paths
 
     def prepare_data(self):
-        image_training_paths, label_training_paths, image_test_paths = self.download_data()
+        image_training_paths, label_training_paths, image_test_paths = self.download_data(
+        )
         self.subjects = []
         self.test_subjects = []
 
-        for image_path, label_path in zip(image_training_paths, label_training_paths):
-            subject = tio.Subject(
-                image=tio.ScalarImage(image_path),
-                label=tio.LabelMap(label_path)
-            )
+        for image_path, label_path in zip(image_training_paths,
+                                          label_training_paths):
+            subject = tio.Subject(image=tio.ScalarImage(image_path),
+                                  label=tio.LabelMap(label_path))
 
             self.subjects.append(subject)
-        
+
         for image_path in image_test_paths:
-            subject = tio.Subject(
-                image=tio.ScalarImage(image_path),
-            )
+            subject = tio.Subject(image=tio.ScalarImage(image_path), )
 
             self.test_subjects.append(subject)
-    
+
     def get_preprocessing_transform(self):
         preprocess = tio.Compose([
             tio.ZNormalization(),
-            tio.CropOrPad(self.get_max_shape(self.subjects + self.test_subjects)),
+            tio.CropOrPad(
+                self.get_max_shape(self.subjects + self.test_subjects)),
             tio.EnsureShapeMultiple(8),  # for the U-Net
             tio.OneHot(),
         ])
         return preprocess
-    
+
     def get_augmentation_transform(self):
         augment = tio.Compose([
             tio.RandomAffine(),
@@ -103,17 +102,26 @@ class DataModule(pl.LightningDataModule):
 
         self.preprocess = self.get_preprocessing_transform()
         augment = self.get_augmentation_transform()
-        self.transform = tio.Compose([self.preprocess, augment])
+        self.transform = tio.Compose([self.preprocess])  #, augment])
 
-        self.train_set = tio.SubjectsDataset(train_subjects, transform=self.transform)
-        self.val_set = tio.SubjectsDataset(val_subjects, transform=self.preprocess)
-        self.test_set = tio.SubjectsDataset(self.test_subjects, transform=self.preprocess)
+        self.train_set = tio.SubjectsDataset(train_subjects,
+                                             transform=self.transform)
+        self.val_set = tio.SubjectsDataset(val_subjects,
+                                           transform=self.preprocess)
+        self.test_set = tio.SubjectsDataset(self.test_subjects,
+                                            transform=self.preprocess)
 
     def train_dataloader(self):
-        return DataLoader(self.train_set, self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.train_set,
+                          self.batch_size,
+                          num_workers=self.num_workers)
 
     def val_dataloader(self):
-        return DataLoader(self.val_set, self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.val_set,
+                          self.batch_size,
+                          num_workers=self.num_workers)
 
     def test_dataloader(self):
-        return DataLoader(self.test_set, self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.test_set,
+                          self.batch_size,
+                          num_workers=self.num_workers)

c_unet/training/lightningUnet.py
@@ -2,8 +2,10 @@ import logging
 import pytorch_lightning as pl
 import torch.nn as nn
 import torchio as tio
+import torch
 
 from torch.optim import Optimizer
+
 
 class LightningUnet(pl.LightningModule):
     """ Lightning model for automation of unet trainingg
@@ -13,41 +15,48 @@ class LightningUnet(pl.LightningModule):
         - optimizer_class (torch.optim.Optimizer):
         - unet (nn.Module): model to use.
         - learning rate (float): learning rate. Defaults to 0.1
+        - gradients_histograms: whether to log all parameters
+            gradients as histograms and the mean of their absolute value as series. 
+            Defaults to False.
     """
-
-    def __init__(self,
-                # Training arguments
-                criterion: nn.Module, 
-                optimizer_class: Optimizer,
-                unet: nn.Module,
-                learning_rate: float = 0.1
-                ):
+    def __init__(
+            self,
+            # Training arguments
+            criterion: nn.Module,
+            optimizer_class: Optimizer,
+            unet: nn.Module,
+            learning_rate: float = 0.001,
+            gradients_histograms: bool = False):
         super(LightningUnet, self).__init__()
 
-        
         self.help_logger = logging.getLogger(__name__)
         self.lr = learning_rate
         self.criterion = criterion
         self.optimizer_class = optimizer_class
         self.unet = unet
         self.is_group = self.unet.group
+        self.gradients_histograms = gradients_histograms
 
         self.save_hyperparameters()
 
     def forward(self, x):
         return self.unet(x)
 
+    def backward(self, loss, optimizer, optimizer_idx, *args, **kwargs):
+        if self.automatic_optimization or self._running_manual_backward:
+            loss.backward(retain_graph=True, *args, **kwargs)
+
     def configure_optimizers(self):
         optimizer = self.optimizer_class(self.parameters(), lr=self.lr)
         return optimizer
-    
+
     def prepare_batch(self, batch):
         if self.is_group:
             image = batch['image'][tio.DATA].unsqueeze(1)
             return image, batch['label'][tio.DATA]
         else:
             return batch['image'][tio.DATA], batch['label'][tio.DATA]
-    
+
     def infer_batch(self, batch):
         inputs, labels = self.prepare_batch(batch)
         outputs = self.forward(inputs)
@@ -58,10 +67,19 @@ class LightningUnet(pl.LightningModule):
         loss = self.criterion(outputs, targets)
         self.log('train_loss', loss, prog_bar=True)
         return loss
-    
+
     def validation_step(self, batch, batch_idx):
         outputs, targets = self.infer_batch(batch)
         loss = self.criterion(outputs, targets)
         self.log('val_loss', loss, prog_bar=True)
         return loss
- 
+
+    def training_epoch_end(self, outputs):
+        if self.gradients_histograms:
+            for name, params in self.named_parameters():
+                if params.grad is not None:
+                    self.logger.experiment.add_histogram(
+                        f"Grad of {name}", params.grad, self.current_epoch)
+                    self.logger.experiment.add_scalar(
+                        f"Mean of abs of grad of {name}",
+                        torch.mean(torch.abs(params.grad)), self.current_epoch)