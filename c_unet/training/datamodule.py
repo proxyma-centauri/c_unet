9fa781f94dd7ec7ba41d16d37da6b3db929c3279
Cécile BONITEAU <proxyma-centauri@users.noreply.github.com>
2021-07-20 16:18:27 +0200

    Commented out some transforms

--- c_unet/training/datamodule.py
+++ c_unet/training/datamodule.py
@@ -76,9 +76,9 @@ class DataModule(pl.LightningDataModule):
     def get_preprocessing_transform(self):
         preprocess = tio.Compose([
             tio.ZNormalization(),
-            tio.CropOrPad(
-                self.get_max_shape(self.subjects + self.test_subjects)),
-            tio.EnsureShapeMultiple(8),  # for the U-Net
+            # tio.CropOrPad(
+            #     self.get_max_shape(self.subjects + self.test_subjects)),
+            # tio.EnsureShapeMultiple(8),  # for the U-Net
             tio.OneHot(),
         ])
         return preprocess
