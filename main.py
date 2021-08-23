eb675781a79b01de2366910ec732b9adae657ab2
Cécile Boniteau <44854391+proxyma-centauri@users.noreply.github.com>
2021-08-23 16:16:18 +0200

    Feat/add flip hippocampus right (#63)
    * Homogenise laterality in datamodule
    
    * Corrected syntax when no subset_name is given
    
    * Wrong condition
    
    * Add normalization config key
    
    * Change default of nonlin
    
    Co-authored-by: Cécile BONITEAU <proxyma-centauri@users.noreply.github.com>

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ def main(logger, args):
                      args.get("OUT_CHANNELS"),
                      final_activation=args.get("FINAL_ACTIVATION"),
                      nonlinearity=args.get("NONLIN"),
+                     normalization=args.get("NORMALIZATION"),
                      divider=args.get("DIVIDER"),
                      model_depth=args.get("MODEL_DEPTH"),
                      dropout=args.get("DROPOUT"))
@@ -60,6 +61,7 @@ def main(logger, args):
                      args.get("OUT_CHANNELS"),
                      final_activation=args.get("FINAL_ACTIVATION"),
                      nonlinearity=args.get("NONLIN"),
+                     normalization=args.get("NORMALIZATION"),
                      divider=args.get("DIVIDER"),
                      model_depth=args.get("MODEL_DEPTH"),
                      dropout=args.get("DROPOUT"))
@@ -239,7 +241,8 @@ if __name__ == "__main__":
     args["IN_CHANNELS"] = 1
     args["OUT_CHANNELS"] = config("OUT_CHANNELS", cast=int)
     args["FINAL_ACTIVATION"] = config("FINAL_ACTIVATION", default="softmax")
-    args["NONLIN"] = config("NONLIN", default="elu")
+    args["NONLIN"] = config("NONLIN", default="leaky-relu")
+    args["NORMALIZATION"] = config("NORMALIZATION", default="bn")
     args["DIVIDER"] = config("DIVIDER", cast=int)
     args["MODEL_DEPTH"] = config("MODEL_DEPTH", cast=int)
     args["DROPOUT"] = config("DROPOUT", cast=float)
